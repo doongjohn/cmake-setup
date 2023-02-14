3024bc9a928c1208d1da6252f3394173191be3e4
doongjohn <doongjohn@gmail.com>
2023-02-14 23:36:59 +0900

    update

--- src/generator.nim
+++ src/generator.nim
@@ -14,13 +14,13 @@ proc generateCmakelistsTxt*(settings: Settings) =
     createDir("cmake")
     let cpmCmakeFile = open("cmake/CPM.cmake", fmWrite)
     defer: cpmCmakeFile.close()
+
     let latestReleaseJson = parseJson(fetch("https://api.github.com/repos/cpm-cmake/CPM.cmake/releases/latest"))
     let cpmLatestVersion = latestReleaseJson["tag_name"].getStr()
     let cpmDownloadUrl = latestReleaseJson["assets"][0]["browser_download_url"].getStr()
-    echo "download: CPM {cpmLatestVersion}".fmt
-    cpmCmakeFile.write(fetch(cpmDownloadUrl))
 
-  var srcExtension = if settings.targetLanguage == "C": "c" else: "cpp"
+    echo "download: CPM {cpmLatestVersion} -> ./cmake/CPM.cmake".fmt()
+    cpmCmakeFile.write(fetch(cpmDownloadUrl))
 
   echo "output: CMakeLists.txt"
   let f = open("CMakeLists.txt", fmWrite)
@@ -31,7 +31,7 @@ proc generateCmakelistsTxt*(settings: Settings) =
 
   # generate compile_commands.json
   set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-  """.dedent)
+  """.dedent())
 
   # TODO: make mingw path configurable
   if settings.useMingw:
@@ -54,7 +54,7 @@ proc generateCmakelistsTxt*(settings: Settings) =
     # search headers and libraries in the target environment
     set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
     set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-    """.dedent)
+    """.dedent())
 
   f.writeLine("""
   project(
@@ -64,44 +64,45 @@ proc generateCmakelistsTxt*(settings: Settings) =
     HOMEPAGE_URL "{settings.projectHomepage}"
     LANGUAGES {settings.targetLanguage}
   )
-  """.fmt.dedent)
+  """.fmt().dedent())
 
   if settings.useCpm:
     f.writeLine("""
     include(cmake/CPM.cmake)
     # https://github.com/cpm-cmake/CPM.cmake/wiki/More-Snippets
-    """.dedent)
+    """.dedent())
 
   # create target
-  f.writeLine("set(TARGET_NAME {settings.targetName})\n".fmt)
+  f.writeLine("set(TARGET_NAME {settings.targetName})\n".fmt())
+  let srcExtension = if settings.targetLanguage == "C": "c" else: "cpp"
   case settings.targetType
   of "exe":
     f.writeLine("""
     file(GLOB_RECURSE SRC_FILES src/*.{srcExtension})
     add_executable(${{TARGET_NAME}} ${{SRC_FILES}})
-    """.fmt.dedent)
+    """.fmt().dedent())
   of "shared-lib":
     f.writeLine("""
     file(GLOB_RECURSE SRC_FILES src/*.{srcExtension})
     add_library(${{TARGET_NAME}} ${{SRC_FILES}})
-    """.fmt.dedent)
+    """.fmt().dedent())
   of "static-lib":
     f.writeLine("""
     file(GLOB_RECURSE SRC_FILES src/*.{srcExtension})
     add_library(${{TARGET_NAME}} STATIC ${{SRC_FILES}})
-    """.fmt.dedent)
+    """.fmt().dedent())
   of "header-only":
     f.writeLine("""
     add_library(${TARGET_NAME} INTERFACE)
-    """.dedent)
+    """.dedent())
 
   # set language standard
   f.writeLine("""
   target_compile_features(${{TARGET_NAME}}
     PRIVATE {settings.targetStandardVersion})
-  """.fmt.dedent)
+  """.fmt().dedent())
 
-  # use sanitizers for debug build
+  # use sanitizers (only for debug build)
   f.writeLine("""
   target_compile_options(${TARGET_NAME}
     PRIVATE
@@ -114,23 +115,23 @@ proc generateCmakelistsTxt*(settings: Settings) =
       $<$<CONFIG:Debug>:-fno-omit-frame-pointer>
       $<$<CONFIG:Debug>:-fno-sanitize-recover=all>
       $<$<CONFIG:Debug>:-fsanitize=address,undefined>)
-  """.dedent)
+  """.dedent())
 
-  # include directories
+  # include directories (commented)
   case settings.targetType
   of "header-only":
     f.writeLine("""
     # target_include_directories(${TARGET_NAME}
     #   INTERFACE ${PROJECT_SOURCE_DIR}/include)
-    """.dedent)
+    """.dedent())
   else:
     f.writeLine("""
     # target_include_directories(${TARGET_NAME}
     #   PRIVATE ${PROJECT_SOURCE_DIR}/include)
-    """.dedent)
+    """.dedent())
 
-  # link libraries
+  # link libraries (commented)
   f.writeLine("""
   # target_link_libraries(${TARGET_NAME}
   #   library_name)
-  """.dedent)
+  """.dedent())
