3aa8ff88e578e4d74ae3b8616053b82834b92997
doongjohn <doongjohn@gmail.com>
2023-11-02 10:15:04 +0900

    update

--- src/main.nim
+++ src/main.nim
@@ -1,27 +1,10 @@
 import std/os
-
-import
-  cli,
-  generator
-
-
-# More Modern CMake
-# https://hsf-training.github.io/hsf-training-cmake-webpage/index.html
-# https://www.youtube.com/watch?v=y7ndUhdQuU8
-
-# Effective Modern CMake
-# https://gist.github.com/mbinna/c61dbb39bca0e4fb7d1f73b0d66a4fd1
-
-# cmake build types
-# https://blog.feabhas.com/2021/07/cmake-part-2-release-and-debug-builds/
-
-# cmake generator expression
-# https://junstar92.tistory.com/214
+import cli, generator
 
 
 proc main =
   if fileExists("CMakeLists.txt"):
-    echo "CMakeLists.txt already exists!"
+    echo "`CMakeLists.txt` already exists in this directory!"
     return
 
   let settings = runInteractivePrompt()
